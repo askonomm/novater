dd840feec62b94eae0d17519b37598ca0c57090b
Asko Nõmm <asko@nth.ee>
2024-09-01 13:16:24 +0300

    Oops, we need to fetch dataset, not booking again. My bad. Also lets check if booking even exists first.

--- domain/booking.py
+++ domain/booking.py
@@ -1,4 +1,5 @@
 from db import Session, Booking
+from domain.search import fetch_dataset
 
 
 def find_booking(booking_id):
@@ -9,6 +10,9 @@ def find_booking(booking_id):
 def can_cancel_booking(booking_id):
     booking = find_booking(booking_id)
 
+    if not booking:
+        return False
+
     # check if a dataset the booking was made with exists,
     # because we can't cancel a booking if the dataset is still available (e.g the last 15)
-    return find_booking(booking.dataset_id) is None
+    return fetch_dataset(booking.dataset_id) is None
